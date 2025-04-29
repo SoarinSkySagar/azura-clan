cea34f2a565bd55c197efe72be4a7852c03c417f
Michelle031 <mbaangel2803@gmail.com>
2025-04-29 10:49:21 +0100

    created next js template

---
+++ client/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to Azura Clan!</h1>
+    </main>
+  );
+}
